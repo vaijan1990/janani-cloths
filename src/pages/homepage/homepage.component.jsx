c0b65ac38d71e086773147ecf7ede5c9da09114e
janani <janani@medinet.se>
2019-08-13 17:10:19 +0200

    broken home page in to components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+  <Directory>
+  </Directory>
+);
+export default HomePage;
