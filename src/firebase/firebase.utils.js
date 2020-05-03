4bde9ca7016152ddd6d16f9ad10694ba8c155297
vaisshnavi  janani <janani@medinet.se>
2020-05-03 09:02:02 +0200

    implemented firebase utils, included ability to store authenticated , finishes sign in method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,8 +11,33 @@ var config = {
     messagingSenderId: "1060627018602",
     appId: "1:1060627018602:web:38256df3c5836a53"
   };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if (!userAuth) return;
+   const userRef  = firestore.doc(`users/${userAuth.uid}`);
+   console.log(userRef);
+   const snapShot = await userRef.get();
+   console.log('1',snapShot);
+
+   if(!snapShot.exists) {
+     const {  displayName, email } = userAuth;
+     const createdAt = new Date();
+
+     try {
+       await userRef.set({
+         displayName,
+         email,
+         createdAt,
+         ...additionalData
+       })
+     } catch (error) {
+       console.log('error creating user', error.message);
+     }
+   }
+   return userRef;
+}
 
 firebase.initializeApp(config);
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
