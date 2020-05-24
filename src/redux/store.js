cb81bb95c18f4f6233d0157ab483e533b0d65479
vaisshnavi  janani <janani@medinet.se>
2020-05-24 12:01:12 +0200

    Implemented container pattern

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 
 if (process.env.NODE_ENV === 'development') {
